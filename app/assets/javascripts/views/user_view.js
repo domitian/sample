61dc35fb9dac27d3bed24b7112926f44d9ec0292
surendra <surendranath@elitmus.com>
2014-03-29 10:40:24 +0530

    temporary save to revert back to

--- app/assets/javascripts/views/user_view.js
+++ app/assets/javascripts/views/user_view.js
@@ -2,47 +2,76 @@
 
 Familybook.UserView = Ember.View.extend({
     templateName: 'user',
-
+    duplicateDrag: [],
     // didInsertElement: function() {
     //     this._super();
-    //     $("#e2").select2({
-    //         minimumInputLength: 0,
-    //         placeholder: "Select a State",
-    //         data: [{
-    //                 id: 1,
-    //                 text: 'bill'
-    //             }, {
-    //                 id: 2,
-    //                 text: 'electricity'
-    //             }
-
-    //         ],
-    //         allowClear: true
-
-    //     });
     // },
-    actions: {
-        // dragForPriority: function(errand, user) {
-        //     var errandList = user.get('recordFormat');
-        //     console.log(errandList.indexOf(errand));
-        // }
-    },
+
     errandView: Ember.View.extend({
         templateName: 'adderrand',
 
     }),
     dashboardView: Ember.View.extend({
         templateName: 'dashboard',
-        dragStart: function(evt) {
+        attributeBindings: 'draggable',
+        draggable: 'true',
+        dragStart: function(event) {
             console.log('draggable');
-            console.log(evt);
-        },
-        drag: function(evt) {
-            console.log('dropping');
-        },
+            console.log(event);
+            var dataTransfer = event.originalEvent.dataTransfer;
+            dataTransfer.setData('Text', this.get('elementId'));
+            console.log(dataTransfer.getData('Text'));
+            this.set('controller.defaultView', false);
+            console.log(this.get('controller.content'));
+            this.set('parentView.duplicateDrag', this.get('recordFormat'));
+            console.log(this.get('parentView.duplicateDrag'));
+            console.log(this.get('content'))
+        }
+        // actions: {
+        //     dragForPriority: function(errand, user, event) {
+        //         var errandList = user.get('recordFormat');
+
+        //         console.log(errandList.indexOf(errand));
+        //         var dataTransfer = event.originalEvent.dataTransfer;
+        //         dataTransfer.setData('Text', this.get('elementId'));
+        //     }
+        // }
+
+    }),
+    testingView: Ember.View.extend({
+        templateName: 'testing',
+
+        attributeBindings: 'draggable',
+        draggable: 'true',
+        dragStart: function(event) {
+            console.log('draggable');
+            var dataTransfer = event.originalEvent.dataTransfer;
+            dataTransfer.setData('Text', this.get('elementId'));
+            console.log(dataTransfer);
+
+        }
+    }),
+    testing1View: Ember.View.extend({
+        templateName: 'testing1',
         dragOver: function(evt) {
             evt.preventDefault();
-            console.log('dropover');
+            console.log('dragover');
+            return false;
+        },
+        dragEnter: function(evt) {
+            evt.preventDefault();
+            console.log('dragEnter');
+            return false;
+        },
+        dragLeave: function(evt) {
+            evt.preventDefault();
+            this.set('controller.defaultView', true);
+            console.log('dragLeave');
+            return false;
+        },
+        drop: function(evt) {
+            console.log('dropped');
+            return false;
         }
     })
 });
